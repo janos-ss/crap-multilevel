2f8069101a3472f756c29e07974a9dd040b852ae
Janos Gyerik <janos.gyerik+windows@sonarsource.com>
2016-11-23 11:10:25 +0100

    Rename HelloWorld classes to make them harder to mistake

--- module1/module11/src/main/java/com/example/crap/multilevel/module1/module11/HelloWorldNested11.java
+++ module1/module11/src/main/java/com/example/crap/multilevel/module1/module11/HelloWorldNested11.java
@@ -1,6 +1,6 @@
 package com.example.crap.multilevel.module1.module11;
 
-public class HelloWorld {
+public class HelloWorldNested11 {
     public static void main(String[] args) {
         System.out.println("hello");
     }
--- module1/modulex/src/main/java/com/example/crap/multilevel/module1/modulex/SayHelloX.java
+++ module1/modulex/src/main/java/com/example/crap/multilevel/module1/modulex/SayHelloX.java
@@ -1,6 +1,6 @@
 package com.example.crap.multilevel.module1.modulex;
 
-public class HelloWorld {
+public class SayHelloX {
     public static void main(String[] args) {
         System.out.println("hello");
     }
